cddc41f06b50ebd79a715d0c0513e27e19a9f825
Shawn Moore <sartak@bestpractical.com>
2008-08-07 00:02:27 +0000

    the API

--- lib/Class/Require.pm
+++ lib/Class/Require.pm
@@ -2,7 +2,27 @@
 package Class::Require;
 use strict;
 use warnings;
+use base 'Exporter';
 
+our @EXPORT_OK = qw/load_class try_load_class is_class_loaded/;
+our %EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+
+sub load_class {
+    my $class = shift;
+
+}
+
+sub try_load_class {
+    my $class = shift;
+
+}
+
+sub is_class_loaded {
+    my $class = shift;
+
+}
 
 1;
 

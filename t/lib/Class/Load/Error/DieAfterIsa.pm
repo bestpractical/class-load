bea4988fd7734e944f3d6d278e9c80da02cc6c98
Dave Rolsky <autarch@urth.org>
2011-09-12 21:00:43 -0500

    Fix comment typos

--- t/lib/Class/Load/Error/DieAfterIsa.pm
+++ t/lib/Class/Load/Error/DieAfterIsa.pm
@@ -5,8 +5,8 @@ use warnings;
 
 # This library emulates a bug that can occur under App::Cmd,
 #
-# A Broken library "use"'s annother library ( App::Cmd::Setup ), and that
-# library injects @ISA during import->()
+# A broken library use's another library ( App::Cmd::Setup ), and that library
+# injects @ISA during import->()
 
 our @ISA = qw( UNIVERSAL );
 
